f0df3605a238c7f80bb055c047300152ca124d67
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-12-05 18:32:05 -0600

    Adds sliding mechanic for the player
    Implements a sliding mechanic when the player presses the down input while on the ground.
    
    The player will now perform a sliding animation and the collision shape is adjusted.
    
    The sliding duration is timed and the player returns to running state after the slide.

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,21 +4,40 @@ extends CharacterBody2D
 const GRAVITY : int = 4200
 const JUMP_VELOCITY = -1800
 
+var is_sliding: bool = false
+var sliding_timer: float = 0.0
+
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
+
+	# Update sliding timer
+	if is_sliding:
+		sliding_timer += delta
+		# Calculate animation duration: 4 frames / 3.0 speed = ~1.33 seconds
+		var animation_duration = 4.0 / 3.0
+		if sliding_timer >= animation_duration:
+			is_sliding = false
+			sliding_timer = 0.0
+			$RunCollisionShape.disabled = false
+
 	if is_on_floor():
 		if not get_parent().game_running:
 			$AnimatedSprite2D.play("idle")
 		else:
-			$RunCollisionShape.disabled = false
 			if Input.is_action_pressed("ui_accept"):
 				velocity.y = JUMP_VELOCITY
-			elif Input.is_action_pressed("ui_down"):
+			elif Input.is_action_just_pressed("ui_down") and not is_sliding:
+				is_sliding = true
+				sliding_timer = 0.0
 				$AnimatedSprite2D.play("sliding")
 				$RunCollisionShape.disabled = true
+			elif is_sliding:
+				# Keep sliding animation playing and collision disabled
+				$AnimatedSprite2D.play("sliding")
 			else:
 				$AnimatedSprite2D.play("running")
+				$RunCollisionShape.disabled = false
 	else:
 		$AnimatedSprite2D.play("jumping")
 
