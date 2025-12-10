6a6e7255661b9fea81eac83e25d3f9c6409d50fe
Rodrigo Gomez Palacio <11739227+rgomezp@users.noreply.github.com>
2025-12-10 14:17:24 -0600

    Add hollywood_2 special event (good)

--- scenes/special_sprite_data.gd
+++ scenes/special_sprite_data.gd
@@ -7,8 +7,11 @@ const SPRITE_QUALITY: Dictionary = {
 	"res://scenes/specials/us_flag.tscn": true,     # true = "makes sense" (good)
 	"res://scenes/specials/cheerleader.tscn": true, # true = "makes sense" (good)
 	"res://scenes/specials/smoker.tscn": true,      # true = "makes sense" (good)
+	"res://scenes/specials/hollywood_2.tscn": true, # true = "makes sense" (good)
+	"res://scenes/specials/rods_bbq.tscn": true,     # true = "makes sense" (good)
 	"res://scenes/specials/devil_plush.tscn": false, # false = "hwwat?" (bad)
 	"res://scenes/specials/man_baby.tscn": false,   # false = "hwwat?" (bad)
+	"res://scenes/specials/go_vegan.tscn": false,   # false = "hwwat?" (bad)
 }
 
 static func is_good_sprite(scene_path: String) -> bool:
